6f3ab31abe62dc016f98e77726eded3725638cf2
Matheus Reusch <matheusreusch1@gmail.com>
2022-01-19 15:48:15 -0300

    Requisito 2

---
+++ src/ProductsList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+class ProductsList extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            products: [],
+        }
+    }
+    
+    render() {
+      const { products } = this.state;  
+        return (
+            <>
+              <input type="text"></input>
+              {products.length === 0 ? <h2 data-testid='home-initial-message'>Digite algum termo de pesquisa ou escolha uma categoria.</h2> : ''}
+            </>
+        )
+    }
+}
+
+export default ProductsList;
