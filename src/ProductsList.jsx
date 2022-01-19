5caa307f8895aef9a1a968124da9c061dee50e93
Matheus Reusch <matheusreusch1@gmail.com>
2022-01-19 16:30:16 -0300

    Requisito 4

--- src/ProductsList.jsx
+++ src/ProductsList.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from './services/api';
 
 class ProductsList extends React.Component {
   constructor() {
     super();
-
     this.state = {
       products: [],
+      categories: [],
     };
   }
 
+  async componentDidMount() {
+    const categorias = await getCategories();
+    console.log(categorias);
+    this.setStateCategories(categorias);
+  }
+
+  setStateCategories = (categorias) => {
+    this.setState({ categories: [...categorias] });
+  };
+
   render() {
-    const { products } = this.state;
+    const { products, categories } = this.state;
     return (
       <>
         <input type="text" />
@@ -20,7 +31,15 @@ class ProductsList extends React.Component {
             <h2 data-testid="home-initial-message">
               Digite algum termo de pesquisa ou escolha uma categoria.
             </h2>
-            <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
+            <Link to="/cart" data-testid="shopping-cart-button">
+              Carrinho
+            </Link>
+            {categories.length > 0
+            && categories.map((categoria) => (
+              <button type="button" key={ categoria.id } data-testid="category">
+                {categoria.name}
+              </button>
+            ))}
           </section>
         ) : (
           ''
