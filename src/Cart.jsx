1f4a8ae7194a8e28e2b1dfc4c7e1496a8a41b599
Matheus Reusch <matheusreusch1@gmail.com>
2022-01-19 16:13:52 -0300

    Requisito 3

---
+++ src/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Cart extends React.Component {
+  render() {
+    return (
+      <section>
+        <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
+      </section>
+    );
+  }
+}
+
+export default Cart;
