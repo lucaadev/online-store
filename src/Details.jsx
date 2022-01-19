3c58ff0136bb04c292cb7b62bdb50bf76a71cd7f
Matheus Reusch <matheusreusch1@gmail.com>
2022-01-19 17:38:04 -0300

    Requisito 7

---
+++ src/Details.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Details extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          produto: {}
+        }
+      }
+    
+      componentDidMount() {
+        const { match } = this.props;
+        const { params } = match;
+        const { id } = params;
+        fetch(`https://api.mercadolibre.com/items/${id}`)
+        .then((response) => response.json())
+        .then((data) => this.setState({ produto: data }));
+      }
+    
+    render() {
+        const { match } = this.props;
+        const { params } = match;
+        const { id } = params;
+        console.log(id);
+        const { produto } = this.state;
+        return (
+          <section>
+            <h2 data-testid="product-detail-name">{produto.title}</h2>
+            <img alt="imagem" src={ produto.thumbnail } />
+            <h2>{produto.price}</h2>
+          </section>
+        )
+    }
+}
+
+export default Details;
